c40ce70b42dc806e5a0eca4e475d7e92fe4ab22c
Sagar Kishore <sagarkishore.ckl@gmail.com>
2019-07-13 13:22:19 +0530

    added start and end variables

--- main.py
+++ main.py
@@ -11,9 +11,11 @@ user_id = 'me'
 labels = get_label_ids(service, label_names)
 message_ids_all_labels = get_all_messages(service, labels, user_id=user_id)
 
-# label = message_ids_all_labels[0]  # ? Zomato
-label = message_ids_all_labels[1]  # ? Swiggy
-final_data = generate_label_json_data(service, user_id, label, start=0, end=-1)
+label = message_ids_all_labels[0]  # ? Zomato
+# label = message_ids_all_labels[1]  # ? Swiggy
+start = 0  # ! 0 to parse from the latest message
+end = -1  # ! -1 to parse till the oldest message
+final_data = generate_label_json_data(service, user_id, label, start=start, end=end)
 
 if label['name'] == 'Swiggy Only':
     path = 'swiggy_data.json'
